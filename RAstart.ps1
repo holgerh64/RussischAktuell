6e05a95dd3509186b00247e858008a83c0600cfe
Holger <110339231+holgerh64@users.noreply.github.com>
2022-11-01 20:53:54 +0100

    Add files via upload

---
+++ RAstart.ps1
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------
+#
+# Startet die Russisch aktuell - Umgebung
+#
+#-------------------------------------------------------------------------
+#
+# Abhängigkeitet:
+#   Russisch aktuell Version 11.2
+#   PowerShell 7.2
+#   OneDrive
+#   Elgato STREAM DECK XL
+#   Elgato STREAM DECK Software-Version: 5.3.3
+#   Advanced Launcher by BarRaider Version 1.7
+#
+#-------------------------------------------------------------------------
+#
+# Autor: Holger Husemann
+# Datum: 01.11.2022
+#
+#-------------------------------------------------------------------------
+cd 'C:\Program Files (x86)\RLei\HyView'
+.\HyView_.exe P=Russ_Akt
+.\REX.EXE
+.\PDIC.EXE
+cd 'C:\Program Files (x86)\SaxEduc\Russ_Akt\'
+cd 'C:\Users\holge\OneDrive\Dokumente\Eigenes zu RLei'
